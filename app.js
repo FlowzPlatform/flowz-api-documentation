6ede0277490b2f4fad9108756a93b4e95c97c166
Ajay Charotariya <acharotariya@officebrain.com>
2018-05-18 12:13:24 +0530

    added docker and ci/cd

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const { PORT } = require('./config.js');
 
 
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.set('port', 3005);
+app.set('port', PORT);
 
 var server = app.listen(app.get('port'), function() {
   var port = server.address().port;
