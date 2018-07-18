e2e994974675c41d0b5af03d63f3bc44ab281db9
Ajay Charotariya <acharotariya@officebrain.com>
2018-07-18 16:26:21 +0530

    updated

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
-module.exports={
-PORT : process.env.PORT
+let config = require("./swagger.json") 
+
+config.host += process.env.HOST
+module.exports = {
+    PORT : process.env.PORT,
+    HOST: process.env.HOST
 };
